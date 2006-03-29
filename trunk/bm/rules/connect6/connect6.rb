2b2b61445e6fc2381e4ab6f2f846f276b7a279e9
Eric Idema <eki@vying.org>
2006-03-29 15:58:55 +0000

    Added new benchmarks for Connect6.init, Connect6.final?, and Connect6.ops.

--- trunk/bm/rules/connect6/connect6.rb
+++ trunk/bm/rules/connect6/connect6.rb
@@ -1,19 +1,33 @@
 require 'benchmark'
 require 'rules/connect6/connect6'
+require 'rules/connectfour/connectfour'
 require 'game'
 
-n=1000
+n=10000
+
+p6 = Connect6.init
 
 Benchmark.bm(12) do |x|
+
+  x.report( "init:" ) do
+    n.times { Connect6.init }
+  end
+
   x.report( "ops:" ) do
+    n.times { Connect6.ops( p6 ) }
+  end
+
+  x.report( "final?:" ) do
+    n.times { Connect6.final?( p6 ) }
+  end
+
+  x.report( "random moves:" ) do
     g = Game.new( Connect6 )
     n.times do
-      ops = g.ops
-      if ops.nil?
+      if g.final?
         g = Game.new( Connect6 )
-        ops = g.ops
       end
-      g << ops[rand(ops.length)]
+      g << g.ops[rand(g.ops.length)]
     end
   end
 end
