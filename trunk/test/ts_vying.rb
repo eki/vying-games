ba512962b2c009a6a0405f5bda0e5256d8a104ab
Eric Idema <eki@vying.org>
2006-03-29 21:54:47 +0000

    Added the new Connect6 unit tests to the main suite.

--- trunk/test/ts_vying.rb
+++ trunk/test/ts_vying.rb
@@ -5,6 +5,7 @@ require "tc_game"
 
 require "board/tc_standard.rb"
 
-require "rules/connectfour/tc_connectfour"
 require "rules/tictactoe/tc_tictactoe"
+require "rules/connectfour/tc_connectfour"
+require "rules/connect6/tc_connect6"
 
