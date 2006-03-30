0e946ceefd04decc0d148f60cf380249f2d60d98
Eric Idema <eki@vying.org>
2006-03-30 01:01:54 +0000

    Added unit tests for Fifteen.

--- trunk/test/ts_vying.rb
+++ trunk/test/ts_vying.rb
@@ -6,6 +6,7 @@ require "tc_game"
 require "board/tc_standard.rb"
 
 require "rules/tictactoe/tc_tictactoe"
+require "rules/tictactoe/tc_fifteen"
 require "rules/connectfour/tc_connectfour"
 require "rules/connect6/tc_connect6"
 
