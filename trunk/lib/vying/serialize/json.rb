ca911fbb7dc9200f0e8d5e0ab3c4f4e4a62e7aa8
Eric Idema <eki@vying.org>
2007-01-06 16:34:34 +0000

    Rewrite of the various to_json methods.  The transformation to_json is now a one-way trip, with no reverse operation available.  The rewrite was focused on reducing the size of the json representations for network transport.

--- trunk/lib/vying/serialize/json.rb
+++ trunk/lib/vying/serialize/json.rb
@@ -3,82 +3,70 @@ require 'vying/game'
 
 class GameResults
   def to_hash
-    { 'rules'         => rules,
+    { 'type'          => 'GameResults',
+      'rules'         => rules,
       'seed'          => seed,
       'sequence'      => sequence,
       'user_map'      => user_map,
       'win_lose_draw' => win_lose_draw,
       'scores'        => scores,
-      'check'         => check }
-  end
-
-  def to_json( *a )
-    { 'json_class' => self.class.name, 'data' => to_hash }.to_json( *a )
+      'check'         => check }.to_json( *a )
   end
 end
 
 class Game
   def to_hash
-    { 'rules'    => rules,
+    { 'type'     => 'Game',
+      'rules'    => rules,
       'history'  => history,
       'sequence' => sequence,
-      'user_map' => user_map }
-  end
-
-  def to_json( *a )
-    { 'json_class' => self.class.name, 'data' => to_hash }.to_json( *a )
+      'user_map' => user_map }.to_json( *a )
   end
 end
 
 class Rules
-  def to_hash
-    hash = {}
-    instance_variables.each do |iv|
-      hash[iv[1,iv.length]] = instance_variable_get( iv )
-    end
-    hash
-  end
-
   def to_json( *a )
-    { 'json_class' => self.class.name, 'data' => to_hash }.to_json( *a )
+    h = { 'type'    => 'Position',
+          'rules'   => class.to_s,
+          'players' => players,
+          'final'   => final? }
+
+    if final?
+      h.merge!( 'winner' => players.select { |p| winner? p },
+                'loser'  => players.select { |p| loser? p },
+                'draw'   => draw? )
+    else
+      h.merge!( 'ops' => ops,
+                'turn' => turn )
+    end
+
+    h.merge!( 'board' => board ) if respond_to? :board
+
+    h.to_json( *a )
   end
 end
 
 class Board
-  def to_hash
-    { 'board' => @board,
-      'coords' => @coords }
-  end
-
   def to_json( *a )
-    { 'json_class' => self.class.name, 'data' => to_hash }.to_json( *a )
-  end
-end
-
-class Coords
-  def to_hash
-    { 'width' => @width,
-      'height' => @height,
-      'coords' => @coords }
+    pieces, board = self.to_a
+    { 'type'   => 'Board',
+      'width'  => coords.width,
+      'height' => coords.height,
+      'pieces' => pieces,
+      'board'  => board }.to_json( *a )
   end
 
-  def to_json( *a )
-    { 'json_class' => self.class.name, 'data' => to_hash }.to_json( *a )
-  end
-end
-
-class Coord
-  def to_hash
-    { 'x' => @x,
-      'y' => @y }
-  end
-
-  def to_json( *a )
-    { 'json_class' => self.class.name, 'data' => to_hash }.to_json( *a )
-  end
-
-  def self.json_create( obj )
-    new( obj['data']['x'], obj['data']['y'] )
+  # Not sure this is an appropriate implementation of to_a
+  def to_a
+    pieces = []
+    array = coords.rows.map do |a| 
+      a.map do |c| 
+        p = self[c]
+        pieces << p unless pieces.include? p
+        pieces.index( p )
+      end 
+    end
+    [pieces, array]
   end
 end
 
