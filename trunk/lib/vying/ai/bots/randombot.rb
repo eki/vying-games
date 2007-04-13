e8571f752ad68996bd29bb1708de5154ff750954
Eric Idema <eki@vying.org>
2007-04-13 00:24:14 +0000

    Brought RandomBot up-to-date.

--- trunk/lib/vying/ai/bots/randombot.rb
+++ trunk/lib/vying/ai/bots/randombot.rb
@@ -2,7 +2,7 @@ require 'vying/ai/bot'
 require 'vying/ai/search'
 
 class RandomBot < AI::Bot
-  def select( position, player )
+  def select( sequence, position, player )
     ops = position.ops
     ops[rand(ops.size)]
   end
