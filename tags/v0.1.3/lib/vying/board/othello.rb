45a3992803266f8ee640df9ff7186b56066e5474
Eric Idema <eki@vying.org>
2007-05-23 00:19:34 +0000

    Tagged version 0.1.3 of the vying project.  Changes include addition of Oware and KeryoPente, improvements to testing, and new ai bots.

---
+++ tags/v0.1.3/lib/vying/board/othello.rb
@@ -0,0 +1,79 @@
+require 'vying/board/board'
+
+class OthelloBoard < Board
+
+  attr_reader :occupied, :frontier
+
+  INITIAL_FRONTIER = [Coord[2,2], Coord[3,2], Coord[4,2], Coord[5,2],
+                      Coord[5,3], Coord[5,4], Coord[5,5], Coord[4,5],
+                      Coord[3,5], Coord[2,5], Coord[2,4], Coord[2,3]]
+
+  def initialize
+    super( 8, 8 )
+
+    @cells[27] = @cells[36] = :white
+    @cells[35] = @cells[28] = :black
+
+    @occupied = { :black => [Coord[3,4], Coord[4,3]],
+                  :white => [Coord[3,3], Coord[4,4]] }
+    @frontier = INITIAL_FRONTIER.dup
+  end
+
+  def initialize_copy( original )
+    super
+    @frontier = original.frontier.dup
+  end
+
+  def clear
+    @frontier.clear
+    super
+  end
+
+#  def valid?( c, bp, directions = [:n,:s,:w,:e,:ne,:nw,:se,:sw] )
+#    return false if !self[c].nil?
+#
+#
+#    op = bp == :black ? :white : :black
+#
+#    a = directions.zip( coords.neighbors_nil( c, directions ) )
+#    a.each do |d,nc|
+#      p = self[nc]
+#      next if p.nil? || p == bp
+#
+#      i = nc
+#      while (i = coords.next( i, d ))
+#        p = self[i]
+#        return true if p == bp 
+#        break       if p.nil?
+#      end
+#    end
+#
+#    false
+#  end
+#
+#  def place( c, bp )
+#    op = bp == :black ? :white : :black
+#
+#    directions = [:n,:s,:w,:e,:ne,:nw,:se,:sw]
+#
+#    a = directions.zip( coords.neighbors_nil( c, directions ) )
+#    a.each do |d,nc|
+#      p = self[nc]
+#      next if p.nil? || p == bp
+#
+#      bt = [nc]
+#      while (bt << coords.next( bt.last, d ))
+#        p = self[bt.last]
+#        break if p.nil?
+#
+#        if p == bp
+#          bt.each { |bc| self[bc] = bp }
+#          break
+#        end
+#      end
+#    end
+#
+#    self[c] = bp
+#  end
+end
+
