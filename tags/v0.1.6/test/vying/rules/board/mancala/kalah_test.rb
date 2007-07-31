8d3b0b0675a56f1f9d7eba4f9d33144abad2ddcc
Eric Idema <eki@vying.org>
2007-07-31 16:03:00 +0000

    Tagged verstion 0.1.6 of vying.  The new code includes rules and ai for Phutball.

---
+++ tags/v0.1.6/test/vying/rules/board/mancala/kalah_test.rb
@@ -0,0 +1,141 @@
+require 'test/unit'
+
+require 'vying'
+require 'vying/rules/rules_test'
+
+class TestKalah < Test::Unit::TestCase
+  include RulesTests
+
+  def rules
+    Kalah
+  end
+
+  def test_initialize
+    g = Game.new( Kalah )
+
+    b = MancalaBoard.new( 6, 2, 4 )
+
+    assert_equal( b, g.board )
+    assert_equal( :one, g.turn )
+    assert_equal( 0, g.score( :one ) )
+    assert_equal( 0, g.score( :two ) )
+  end
+
+  def test_ops
+    g = Game.new( Kalah )
+
+    assert_equal( :one, g.turn )
+    assert_equal( ['a1', 'b1', 'c1', 'd1', 'e1', 'f1'], g.ops )
+
+    g << :a1
+
+    assert_equal( :two, g.turn )
+    assert_equal( ['a2', 'b2', 'c2', 'd2', 'e2', 'f2'], g.ops )
+
+    g << :f2
+
+    assert_equal( :one, g.turn )
+    assert_equal( ['b1', 'c1', 'd1', 'e1', 'f1'], g.ops )
+
+    g << :e1
+
+    assert_equal( :one, g.turn )
+    assert_equal( ['a1', 'b1', 'c1', 'd1', 'f1'], g.ops )
+
+    while ops = g.ops do
+      g << ops[0]
+    end
+
+    assert_not_equal( g.history[0], g.history.last )
+  end
+
+  def test_players
+    g = Game.new( Kalah )
+    assert_equal( [:one,:two], g.players )
+  end
+
+  def test_has_score
+    g = Game.new( Kalah )
+    g << g.ops.first
+
+    assert( g.has_score? )
+    assert_equal( 1, g.score( :one ) )
+    assert_equal( 0, g.score( :two ) )
+  end
+
+  def test_capture
+    g = Game.new( Kalah )
+    g << :a1 << :f2
+
+    assert_equal( 1, g.score( :one ) )
+    assert_equal( 5, g.board[:a2] )
+    assert_equal( 0, g.board[:a1] )
+    assert_equal( 5, g.board[:f1] )
+
+    g << :f1
+
+    assert_equal( 7, g.score( :one ) )
+    assert_equal( 0, g.board[:a2] )
+    assert_equal( 0, g.board[:a1] )
+    assert_equal( 0, g.board[:f1] )
+  end
+
+  def test_no_capture
+    g = Game.new( Kalah )
+
+    g << :a1 << :a2
+
+    assert_equal( 1, g.score( :one ) )
+    assert_equal( 0, g.board[:a2] )
+    assert_equal( 0, g.board[:a1] )
+    assert_equal( 4, g.board[:c1] )
+
+    g << :c1
+
+    assert_equal( 2, g.score( :one ) )
+    assert_equal( 1, g.board[:a2] )
+    assert_equal( 1, g.board[:a1] )
+    assert_equal( 0, g.board[:c1] )
+  end
+
+  def test_extra_turn
+    g = Game.new( Kalah )
+    g << :a1 << :f2
+    
+    assert_equal( :one, g.turn )
+
+    g << :e1
+
+    assert_equal( :one, g.turn )
+  end
+
+  def test_final
+    g = Game.new( Kalah )
+
+    # Doctor the board
+
+    g.board[:b1,:c1,:d1,:e1,:f1] = 0
+
+    assert( !g.final? )
+    assert_equal( 0, g.score( :one ) )
+    assert_equal( 0, g.score( :two ) )
+    assert_equal( ['a1'], g.ops )
+
+    g << :a1
+
+    assert( !g.final? )  # :one's side is empty, but player two can still move
+
+    g << :a2
+
+    assert( g.final? )
+    assert_equal( 1, g.score( :one ) )
+    assert_equal( 27, g.score( :two ) )
+    assert( !g.winner?( :one ) )
+    assert( g.winner?( :two ) )
+    assert( g.loser?( :one ) )
+    assert( !g.loser?( :two ) )
+    assert( !g.draw? )
+  end
+
+end
+
