74c57431f47b76a2a89b4d1d7185819ad3f43dbe
Eric Idema <eki@vying.org>
2007-11-15 21:46:23 +0000

    Moved the "strategy" support code from bots/ to strategies/ for further refactoring.

--- trunk/lib/vying/ai/bots/randombot.rb
+++ trunk/lib/vying/ai/bots/randombot.rb
@@ -1,7 +1,7 @@
 require 'vying/ai/bot'
 require 'vying/ai/search'
 
-class RandomBot < AI::Bot
+class RandomBot < Bot
   def select( sequence, position, player )
     moves = position.moves
     moves[rand(moves.size)]
