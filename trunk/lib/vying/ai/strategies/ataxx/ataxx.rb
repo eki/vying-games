95b6eb0fdb9cfa8433922344692da9eeffc30701
Eric Idema <eki@vying.org>
2007-11-16 01:06:11 +0000

    Refactored Ataxx bots into new structure.

--- trunk/lib/vying/ai/strategies/ataxx/ataxx.rb
+++ trunk/lib/vying/ai/strategies/ataxx/ataxx.rb
@@ -1,6 +1,6 @@
 require 'vying/ai/bot'
 
-module AI::Ataxx
+module AtaxxStrategies
 
   def eval_score( position, player )
     opp = player == :red ? :blue : :red
@@ -63,63 +63,5 @@ module AI::Ataxx
     score
   end
 
-  module Bot
-    include AI::Ataxx
-    include AlphaBeta
-
-    attr_reader :nodes, :leaf
-
-    def initialize
-      super
-    end
-
-    def select( sequence, position, player )
-      return position.moves.first if position.moves.length == 1
-
-      t = position.turn
-      p = position.apply( position.moves.first )
-      return position.moves.first if p.turn == t
-
-      @leaf, @nodes = 0, 0
-      score, move = fuzzy_best( analyze( position, player ), 0 )
-      puts "**** Searched #{nodes}:#{leaf} positions, best: #{score}"
-
-      move
-    end
-
-    def evaluate( position, player )
-      @leaf += 1
-
-      return  1000 if position.final? && position.winner?( player )
-      return -1000 if position.final? && position.loser?( player )
-      return     0 if position.final?
-
-      eval( position, player )
-    end
-
-    def cutoff( position, depth )
-      position.final? || depth >= 4
-    end
-
-    def prune( position, player, moves )
-      jumps = []
-      splits = {}
-
-      moves.each do |m|
-        coords = m.to_coords
-
-        dx = (coords.first.x - coords.last.x).abs
-        dy = (coords.first.y - coords.last.y).abs
-
-        if dx <= 1 && dy <= 1 && (dx == 1 || dy == 1)
-          splits[coords.last] = m
-        else
-          jumps << m
-        end
-      end
-
-      splits.values + jumps
-    end
-  end
 end
 
