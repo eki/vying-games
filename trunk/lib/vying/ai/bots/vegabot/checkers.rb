874343b1e20014f63e838fc5fddd54733f7cc990
Eric Idema <eki@vying.org>
2007-11-16 01:20:55 +0000

    Refactored Checkers bots into new structure.

---
+++ trunk/lib/vying/ai/bots/vegabot/checkers.rb
@@ -0,0 +1,47 @@
+require 'vying/ai/bot'
+require 'vying/ai/search'
+require 'vying/ai/strategies/checkers/checkers'
+
+class VegaBot < Bot
+  class Checkers < Bot
+    include AlphaBeta
+    include CheckersStrategies
+
+    attr_reader :leaf, :nodes
+
+    def initialize
+      super
+      @leaf, @nodes = 0, 0
+      load_openings
+    end
+
+    def select( sequence, position, player )
+      return position.moves.first if position.moves.length == 1
+
+      @leaf, @nodes = 0, 0
+      score, move = fuzzy_best( analyze( position, player ), 1 )
+      puts "**** Searched #{nodes}:#{leaf} positions, best: #{score}"
+      move
+    end
+
+    def accept_draw?( sequence, position, player )
+      position.board.occupied[player].length < 3
+    end
+
+    def evaluate( position, player )
+      @leaf += 1
+
+      return  1000 if position.final? && position.winner?( player )
+      return -1000 if position.final? && position.loser?( player )
+      return     0 if position.final?
+    
+      eval_captures( position, player )
+    end
+
+    def cutoff( position, depth )
+      position.final? || depth >= 2
+    end
+
+  end
+end
+
