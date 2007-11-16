4b66d3c70b187112b0b9aff84d152379414f3a9c
Eric Idema <eki@vying.org>
2007-11-16 02:19:06 +0000

    Refactored NineMensMorris bots into new directory structure.

---
+++ trunk/lib/vying/ai/bots/capellabot/nine_mens_morris.rb
@@ -0,0 +1,42 @@
+require 'vying/ai/bot'
+require 'vying/ai/search'
+require 'vying/ai/strategies/nine_mens_morris/nine_mens_morris'
+
+class CapellaBot < Bot
+  class NineMensMorris < Bot
+    include AlphaBeta
+    include NineMensMorrisStrategies
+
+    attr_reader :leaf, :nodes
+
+    def initialize
+      super
+      @leaf, @nodes = 0, 0
+    end
+
+    def select( sequence, position, player )
+      return position.moves.first if position.moves.length == 1
+
+      @leaf, @nodes = 0, 0
+      score, move = fuzzy_best( analyze( position, player ), 1 )
+      puts "**** Searched #{nodes}:#{leaf} positions, best: #{score}"
+      move
+    end
+
+    def evaluate( position, player )
+      @leaf += 1
+
+      return  1000 if position.final? && position.winner?( player )
+      return -1000 if position.final? && position.loser?( player )
+      return     0 if position.final?
+    
+      eval_score( position, player )
+    end
+
+    def cutoff( position, depth )
+      position.final? || depth >= 0
+    end
+
+  end
+end
+
---
+++ trunk/lib/vying/ai/bots/siriusbot/nine_mens_morris.rb
@@ -0,0 +1,54 @@
+require 'vying/ai/bot'
+require 'vying/ai/search'
+require 'vying/ai/strategies/nine_mens_morris/nine_mens_morris'
+
+class SiriusBot < Bot
+  class NineMensMorris < Bot
+    include AlphaBeta
+    include NineMensMorrisStrategies
+
+    attr_reader :leaf, :nodes
+
+    def initialize
+      super
+      @leaf, @nodes = 0, 0
+    end
+
+    def select( sequence, position, player )
+      return position.moves.first if position.moves.length == 1
+
+      @leaf, @nodes = 0, 0
+      score, move = fuzzy_best( analyze( position, player ), 1 )
+      puts "**** Searched #{nodes}:#{leaf} positions, best: #{score}"
+      move
+    end
+
+    def evaluate( position, player )
+      @leaf += 1
+
+      return  1000 if position.final? && position.winner?( player )
+      return -1000 if position.final? && position.loser?( player )
+      return     0 if position.final?
+    
+      eval_score( position, player ) +
+      eval_mills( position, player ) +
+      eval_mobility( position, player )
+    end
+
+    def cutoff( position, depth )
+      return true if position.final?
+
+      if position.moves.length < 2
+        depth >= 5
+      elsif position.moves.length < 4
+        depth >= 4
+      elsif position.moves.length > 12
+        depth >= 1
+      else
+        depth >= 2
+      end
+    end
+
+  end
+end
+
