d8f9e0305901426b9c88dd525dca9299b203363e
Eric Idema <eki@vying.org>
2007-11-26 00:43:11 +0000

    Added unit test for #hash.

--- trunk/test/vying/rules/board/phutball_test.rb
+++ trunk/test/vying/rules/board/phutball_test.rb
@@ -223,6 +223,19 @@ class TestPhutball < Test::Unit::TestCase
     assert( ! g.moves.include?( "k14" ) )
   end
 
+  def test_hash
+    g1 = Game.new Phutball
+    g2 = Game.new Phutball
+
+    10.times do
+      g1 << g1.moves.first
+      g2 << g2.moves.first
+    end
+
+    assert( g1.history.last == g2.history.last )
+    assert( g1.history.last.hash == g2.history.last.hash )
+  end
+
   def test_game01
     g = play_sequence( [:h10,:h9,:h8,:h7,:h6,:h5,:h4,:h3,:h2,:h11h1] )
 
@@ -242,28 +255,6 @@ class TestPhutball < Test::Unit::TestCase
     assert( !g.winner?( :ohs ) )
     assert( g.loser?( :ohs ) )
   end
-
-# def test_game02
-#   # This game is going to be a win for White (diagonal)(winner in middle)
-#   g = play_sequence( [:f13,:a1,:c3,:f12,:f11,:d4,:e5,:f14,:f10,:f6,:b2] )
-
-#   assert( !g.draw? )
-#   assert( !g.winner?( :black ) )
-#   assert( g.loser?( :black ) )
-#   assert( g.winner?( :white ) )
-#   assert( !g.loser?( :white ) )
-# end
-
-# def test_game03
-#   # This game is going to be a win for Black (horizontal)(7-in-a-row)
-#   g = play_sequence [:a1,:f10,:f9,:b1,:c1,:g10,:g9,:e1,:f1,:g8,:g7,:g1,:d1]
-
-#   assert( !g.draw? )
-#   assert( g.winner?( :black ) )
-#   assert( !g.loser?( :black ) )
-#   assert( !g.winner?( :white ) )
-#   assert( g.loser?( :white ) )
-# end
 
 end
 
