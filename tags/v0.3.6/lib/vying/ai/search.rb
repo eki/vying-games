35eab76ce63456ead4bae0c7e8048b7402017268
Eric Idema <eki@vying.org>
2008-02-07 00:14:30 +0000

    Removing several old versions of vying.

--- tags/v0.3.6/lib/vying/ai/search.rb
+++
@@ -1,6 +0,0 @@
-# Copyright 2007, Eric Idema except where otherwise noted.
-# You may redistribute / modify this file under the same terms as Ruby.
-
-require 'vying/ai/search/minimax'
-require 'vying/ai/search/alphabeta'
-
