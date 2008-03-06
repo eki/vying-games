2b0a06c3b0b8b6993e4eb89fbedcbf66e6c4b95e
Eric Idema <eki@vying.org>
2008-03-06 19:24:44 +0000

    Ahem, tagged v0.5.1.1 ... the same as v0.5.1 but it actually passes the unit tests.

---
+++ tags/v0.5.1.1/lib/vying/cli/info.rb
@@ -0,0 +1,34 @@
+# Copyright 2007, Eric Idema except where otherwise noted.
+# You may redistribute / modify this file under the same terms as Ruby.
+
+require 'optparse'
+require 'vying'
+
+CLI::SUBCOMMANDS << "info"
+
+module CLI
+
+  def CLI.info
+
+    rules = []
+    key = nil
+
+    opts = OptionParser.new
+    opts.banner = "Usage: vying info [options]"
+    opts.on( "-r", "--rules RULES" ) { |r| rules << Rules.find( r ) }
+    opts.on( "-k", "--key KEY"     ) { |k| key = k }
+
+    opts.parse( ARGV )
+
+    rules = Rules.list if rules.empty?
+
+    rules.each do |r|
+      if key.nil?
+        r.info.each_pair { |k,v| puts "#{k}: #{v}" }
+      else
+        puts r.info[key]
+      end
+    end
+  end
+end
+
