066a46b6f7076ba9cfc32e917425e77023d14f71
Eric K Idema <eki@vying.org>
2008-09-10 14:35:20 -0400

    Refactored / cleaned up cli/bench.  Stubbed it out to include two new benchmarks (for Game and Board).

--- lib/vying/cli/bench.rb
+++ lib/vying/cli/bench.rb
@@ -10,134 +10,119 @@ CLI::SUBCOMMANDS << "bench"
 module CLI
   
   def CLI.bench
+    h = { 
+      :rules       => [],
+      :exclude     => [],
+      :n           => 100,
+      :clear_cache => false
+    }
 
-    rules = []
-    exclude = []
-    n = 100
-    benchmark_game = false
-    benchmark_marshal = false
-    clear_cache = false
+    benchmark = :position
 
     opts = OptionParser.new
     opts.banner = "Usage: vying bench [options]"
-    opts.on( "-r", "--rules RULES"   ) { |r| rules << Rules.find( r ) }
-    opts.on( "-e", "--exclude RULES" ) { |r| exclude << Rules.find( r ) }
-    opts.on( "-n", "--number NUMBER" ) { |num| n = Integer( num ) }
-    opts.on( "-g", "--game" )          { benchmark_game = true }
-    opts.on( "-m", "--marshal" )       { benchmark_marshal = true }
-    opts.on( "-p", "--profile"       ) { require 'profile' }
-    opts.on( "-c", "--clear-cache"   ) { clear_cache = true }
+    opts.on( "-r", "--rules RULES"   ) { |r| h[:rules]   << Rules.find( r )  }
+    opts.on( "-e", "--exclude RULES" ) { |r| h[:exclude] << Rules.find( r )  }
+    opts.on( "-n", "--number NUMBER" ) { |n| h[:n] = n.to_i                  }
+    opts.on( "-g", "--game"          ) { benchmark = :game                   }
+    opts.on( "-b", "--board"         ) { benchmark = :board                  }
+    opts.on( "-p", "--profile"       ) { require 'profile'                   }
+    opts.on( "-c", "--clear-cache"   ) { h[:clear_cache] = true              }
 
     opts.parse( ARGV )
 
-    rules = Rules.latest_versions if rules.empty?
-    exclude.each { |r| rules.delete( r ) }
+    if benchmark == :position || benchmark == :game
+      h[:rules] = Rules.latest_versions if h[:rules].empty?
+      h[:exclude].each { |r| h[:rules].delete( r ) }
+    end
 
-    Benchmark.bm( 30 ) do |x|
-      rules.each do |r|
-        pos = r.new
-        move = pos.moves.first
-        p_a = Array.new( n ) { |i| pos.dup }
+    CLI::Bench.send( benchmark, h )
+  end
 
-        x.report( "#{r} position dup" ) do
-          n.times { pos.dup }
-        end
+  module Bench
 
-        g, i = Game.new( r ), 0
-        until g.final? || i == 30
-          g << g.moves.first
-          i += 1
-        end
-        marshal_p = g.history.last
-        marshal_g = g
-        marshal_s = Marshal.dump( marshal_p )
-        marshal_gs = Marshal.dump( marshal_g )
+    def self.position( h )
+      n = h[:n]
 
-        x.report( "#{r} marshal dump" ) do
-          n.times { Marshal.dump( marshal_p ) }
-        end
+      Benchmark.bm( 30 ) do |x|
+        h[:rules].each do |r|
+          positions, moves, players = [], [], []
 
-        x.report( "#{r} marshal load" ) do
-          n.times { Marshal.load( marshal_s ) }
-        end
-
-        if benchmark_game
-          x.report( "#{r} (game) marshal dump" ) do
-            n.times { Marshal.dump( marshal_g ) }
-          end
-
-          x.report( "#{r} (game) marshal load" ) do
-            n.times { Marshal.load( marshal_gs ) }
-          end
-        end
-
-        x.report( "#{r} init" ) do
-          n.times { r.new }
-        end
-
-        p = r.new
-        p_a[0] = [p, p.moves[rand( p.moves.length )]]
-        (p_a.length - 1).times do |i|
-          p = p_a[i].first.apply( p_a[i].last )
-          p = r.new if p.final?
-          p_a[i+1] = [p, p.moves[rand( p.moves.length )]]
-        end
-
-        p_a.each { |p,m| p.clear_cache } if clear_cache
-
-        x.report( "#{r} move?" ) do
-          p_a.each { |p,m| p.move?( m ) }
-        end
-
-        p_a.each { |p,m| p.clear_cache } if clear_cache
-
-        x.report( "#{r} has_moves" ) do
-          p_a.each { |p,m| p.has_moves }
-        end
-
-        p_a.each { |p,m| p.clear_cache } if clear_cache
-
-        x.report( "#{r} moves" ) do
-          p_a.each { |p,m| p.moves }
-        end
-
-        p_a.each { |p,m| p.clear_cache } if clear_cache
-
-        x.report( "#{r} apply" ) do
-          p_a.each { |p,m| p.apply( m ) }
-        end
-
-        p_a.each { |p,m| p.clear_cache } if clear_cache
-
-        x.report( "#{r} final?" ) do
-          p_a.each { |p,m| p.final? }
-        end
-
-        x.report( "#{r} random play" ) do
           p = r.new
-          n.times do
-            p = r.new if p.final?
-            p.apply!( p.moves[rand( p.moves.length )] )
+          n.times do |i|
+            positions[i] = p
+
+            if p.final?
+              moves[i], players[i] = nil, nil
+              p = r.new
+            else
+              players[i] = p.has_moves[rand( p.has_moves.length )]
+              moves[i] = 
+                p.moves( players[i] )[rand( p.moves( players[i] ).length )]
+
+              p = p.apply( moves[i], players[i] )
+            end
+          end
+
+          x.report( "#{r} init" ) do
+            n.times { r.new }
+          end
+
+          x.report( "#{r} position dup" ) do
+            n.times { |i| positions[i].dup }
+          end
+
+          positions.each { |p| p.clear_cache } if h[:clear_cache]
+
+          x.report( "#{r} move?" ) do
+            n.times { |i| positions[i].move?( moves[i], players[i] ) }
+          end
+
+          positions.each { |p| p.clear_cache } if h[:clear_cache]
+
+          x.report( "#{r} has_moves" ) do
+            n.times { |i| positions[i].has_moves }
+          end
+
+          positions.each { |p| p.clear_cache } if h[:clear_cache]
+
+          x.report( "#{r} moves" ) do
+            n.times { |i| positions[i].moves( players[i] ) }
+          end
+
+          positions.each { |p| p.clear_cache } if h[:clear_cache]
+
+          x.report( "#{r} apply" ) do
+            n.times do |i| 
+              positions[i].apply( moves[i], players[i] ) if moves[i]
+            end
+          end
+
+          positions.each { |p| p.clear_cache } if h[:clear_cache]
+
+          x.report( "#{r} final?" ) do
+            n.times { |i| positions[i].final? }
+          end
+
+          x.report( "#{r} random play" ) do
+            p = r.new
+            n.times do
+              p = r.new if p.final?
+              player = p.has_moves[rand( p.has_moves.length )]
+              move = p.moves( player )[rand( p.moves( player ).length )] 
+              p.apply!( move, player )
+            end
           end
         end
       end
     end
 
-    if benchmark_marshal
-      rules.each do |r|
+    def self.game( h )
 
-        g, i = Game.new( r ), 0
-        until g.final? || i == 30
-          g << g.moves.first
-          i += 1
-        end
-        marshal_p = g.history.last
-        marshal_g = g
-        marshal_s = Marshal.dump( marshal_p )
-        marshal_gs = Marshal.dump( marshal_g )
+    end
 
-        puts "#{r} marshal dump size: #{marshal_s.length}"
-      end
+    def self.board( h )
+
     end
 
   end
