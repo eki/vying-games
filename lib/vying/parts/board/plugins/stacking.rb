cb327c81719d74c9e87168db8d38808c7d2581c2
Eric K Idema <eki@vying.org>
2008-09-12 13:31:48 -0400

    Added stub for a Stacking board plugin.

---
+++ lib/vying/parts/board/plugins/stacking.rb
@@ -0,0 +1,17 @@
+# Copyright 2007, Eric Idema except where otherwise noted.
+# You may redistribute / modify this file under the same terms as Ruby.
+
+require 'vying'
+
+# This is a stub for a plugin to make it easier to stack pieces on a board.
+
+module Board::Plugins::Stacking
+
+  # Display the board in such a way that entire stacks can be seen.
+
+  def to_s
+    super  # Change me
+  end
+
+end
+
