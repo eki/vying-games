2c79609b7ac4537dfcff02658ede18d9516b4ddc
Eric K Idema <eki@vying.org>
2008-12-07 17:00:10 -0500

    Major refactor of http/server login code.  Added 'login' cli tool to login to the server and check login status.

--- lib/vying/http/server.rb
+++ lib/vying/http/server.rb
@@ -1,11 +1,15 @@
 
 
 class Vying::Server
-  attr_reader :host, :port, :session, :auth_token, :username
+  attr_reader :host, :port, :session, :username, :auth_token
 
-  def initialize( host, port, username )
-    @host, @port, @username = host, port, username
-    @logged_in = true
+  def initialize( params={} )
+    @host, @port, @username = params[:host], params[:port], params[:username]
+    @auth_token = params[:auth_token]
+    
+    @logged_in = !! @auth_token
+
+    @debug = params[:debug]
   end
 
   def login( password, remember=false )
@@ -29,6 +33,10 @@ class Vying::Server
     @logged_in
   end
 
+  def debug?
+    !! @debug
+  end
+
   def update_cookies( cookies )
     h = {}
     cookies.each do |c|
@@ -36,11 +44,15 @@ class Vying::Server
       h[n] = v
     end
 
-    @session    = h['_session_id'] if h['_session_id'] != session
-    @auth_token = h['auth_token']  if h['auth_token']  != auth_token
+    @session = h['_session_id']  if h['_session_id'] != session
+
+    if h['auth_token'] != auth_token
+      @auth_token = h['auth_token']
+      save_auth_token
+    end
   end
 
-  def get( path, params )
+  def get( path, params={} )
     unless params.empty?
       path += "?"
       path += params.map { |n,v| "#{n}=#{v}" }.join( "&" )
@@ -56,6 +68,10 @@ class Vying::Server
     response = http.get( path, headers )
 
     if response.code == "200"
+      update_cookies( response.get_fields( 'set-cookie' ) )
+
+      open( "./last.yaml", "w" ) { |f| f.puts response.body }  if debug?
+
       YAML.load( response.body )
     else
       nil
@@ -71,17 +87,88 @@ class Vying::Server
     s != "" ? s : nil
   end
 
+  def save_auth_token
+    self.class.create_cookie_dir
+
+    fn = File.expand_path( 
+      "~/.vying/cookies/#{username}_#{host}_#{port}_auth_token.txt" )
+
+    open( fn, "w" ) do |f|
+      f.puts "auth_token=#{auth_token};"
+    end
+  end
+
+  def delete_auth_token
+    self.class.create_cookie_dir
+
+    fn = File.expand_path( 
+      "~/.vying/cookies/#{username}_#{host}_#{port}_auth_token.txt" )
+
+    File.delete( fn )  if File.exists?( fn )
+  end
+
   class << self
     attr_reader :current
 
-    def connect( host, port, username, password=nil )
-      @current = self.new( host, port, username )
-      @current.login( password )  if password
+    def connect( params={} )
+      params.merge!( load_auth_token( params ) )
+
+      @current = new( params )
+
+      yield @current
+
       @current
     end
 
-    def get( path, params )
-      current.get( path, params )
+    def get( path, params={} )
+      current.get( path, params={} )
+    end
+
+    def create_vying_dir
+      d = File.expand_path( "~/.vying" )
+
+      unless File.exists?( d )
+        old_mask = File.umask( 0022 )
+        Dir.mkdir( d )
+        File.umask( old_mask )
+      end
+    end
+
+    def create_cookie_dir
+      create_vying_dir
+
+      d = File.expand_path( "~/.vying/cookies" )
+
+      unless File.exists?( d )
+        old_mask = File.umask( 0077 )
+        Dir.mkdir( d )
+        File.umask( old_mask )
+      end
+    end
+
+    def load_auth_token( params={} )
+      create_cookie_dir
+
+      host = params[:host] || '*'
+      port = params[:port] || '*'
+      username = params[:username] || '*'
+
+      d = File.expand_path( "~/.vying/cookies" )
+
+      fn = Dir.glob( "#{d}/#{username}_#{host}_#{port}_auth_token.txt" ).first
+
+      h = {}
+
+      if fn
+        h[:auth_token] = File.open( fn, "r" ).read.split( /[=;\n]/ ).last
+        if fn =~ /\/(\w+)_(.+)_(\d+)_auth_token.txt/
+          h[:username] = $1
+          h[:host] = $2
+          h[:port] = $3.to_i
+        end
+      end
+
+      h
     end
   end
 end
