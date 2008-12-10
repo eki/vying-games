fae4ea66b602bd8f277f9948248ebce79fdcc375
Eric K Idema <eki@vying.org>
2008-12-09 21:07:36 -0500

    Added four game tests for Spangles.

--- test/vying/rules/tile/spangles_test.rb
+++ test/vying/rules/tile/spangles_test.rb
@@ -37,5 +37,49 @@ class TestSpangles < Test::Unit::TestCase
     g << g.moves.first
     assert_equal( [:white], g.has_moves )
   end
+
+  def test_game01   # Quick, simple win
+    g = play_sequence %w( (-1,0) (-1,-1) (0,1) (-2,-1) (1,0) )
+
+    assert( !g.draw? )
+    assert( g.winner?( :black ) )
+    assert( !g.loser?( :black ) )
+    assert( !g.winner?( :white ) )
+    assert( g.loser?( :white ) )
+  end
+
+  def test_game02   # Suicide
+    g = play_sequence %w( (1,0) (1,-1) (2,0) (0,-1) (3,0) (-1,-1) (-2,-1) 
+                          (-3,-1) (-3,0) (-2,0) (-1,0) )
+
+    assert( !g.draw? )
+    assert( !g.winner?( :black ) )
+    assert( g.loser?( :black ) )
+    assert( g.winner?( :white ) )
+    assert( !g.loser?( :white ) )
+  end
+
+  def test_game03   # Win by filling center
+    g = play_sequence %w( (1,0) (1,-1) (2,0) (0,-1) (3,0) (-1,-1) (-2,-1) 
+                          (-3,-1) (-3,0) (-2,0) (4,0) (-1,0) )
+
+    assert( !g.draw? )
+    assert( !g.winner?( :black ) )
+    assert( g.loser?( :black ) )
+    assert( g.winner?( :white ) )
+    assert( !g.loser?( :white ) )
+  end
+
+  def test_game04   # Simultaneous connection
+    g = play_sequence %w( (1,0) (1,-1) (0,-1) (-1,-1) (-2,-1) (-3,-1) (-3,0)
+                          (-2,0) (-1,0) )
+
+    assert( !g.draw? )
+    assert( g.winner?( :black ) )
+    assert( !g.loser?( :black ) )
+    assert( !g.winner?( :white ) )
+    assert( g.loser?( :white ) )
+  end
+
 end
 
