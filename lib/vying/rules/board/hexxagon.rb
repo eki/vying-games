fbfc5467014d92d5dd1ee920a305a7d3889c4f21
Eric K Idema <eki@vying.org>
2008-12-24 12:56:21 -0500

    Ruby 1.9 doesn't seem to like the use of splat here.  The board gets corrupted (arrays instead of pieces).  This fixes the failing unit test.

--- lib/vying/rules/board/hexxagon.rb
+++ lib/vying/rules/board/hexxagon.rb
@@ -107,7 +107,8 @@ Rules.create( "Hexxagon" ) do
 
       m.keys.sort_by { |c| c.to_s }.sort_by { rand }.each do |c|
         if n - 1 - m[c].length >= 0
-          board[c] = board[*m[c]] = :x
+          board[c] = :x
+          m[c].each { |mc| board[mc] = :x }
           n -= (1 + m[c].length)
         end
 
