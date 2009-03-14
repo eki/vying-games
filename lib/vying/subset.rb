8cae75e9f20abddfa0ccaf74eb06708a68797e39
Eric K Idema <eki@vying.org>
2009-03-14 18:47:13 -0400

    Wrap in module Vying.

--- lib/vying/subset.rb
+++ lib/vying/subset.rb
@@ -5,31 +5,33 @@ require 'vying'
 
 # Utility methods related to counting subsets.
 
-module Vying::Subset
-  class << self
+module Vying
+  module Subset
+    class << self
 
-    def factorial( n )
-      return 1  if n < 2
+      def factorial( n )
+        return 1  if n < 2
 
-      (1..n).inject { |a,b| a * b }
+        (1..n).inject { |a,b| a * b }
+      end
+
+      def count_subsets( n, m ) 
+        return 1   if n == 0
+
+        factorial( m ) / (factorial( n ) * factorial( m - n )) 
+      end
+
+      def count_subsets_less_than( n, m )
+        (0..n).inject( 0 ) { |a,b| a + count_subsets( b, m ) }
+      end
+
+      extend Memoizable
+
+      memoize :factorial
+      memoize :count_subsets
+      memoize :count_subsets_less_than
+
     end
-
-    def count_subsets( n, m ) 
-      return 1   if n == 0
-
-      factorial( m ) / (factorial( n ) * factorial( m - n )) 
-    end
-
-    def count_subsets_less_than( n, m )
-      (0..n).inject( 0 ) { |a,b| a + count_subsets( b, m ) }
-    end
-
-    extend Memoizable
-
-    memoize :factorial
-    memoize :count_subsets
-    memoize :count_subsets_less_than
-
   end
 end
 
