2281db143e8d529a3d4c66be6a7059a7dc43ca49
Eric K Idema <eki@vying.org>
2009-03-14 20:38:30 -0400

    Updated http extensions to Position (namespace change).

--- lib/vying/http/position.rb
+++ lib/vying/http/position.rb
@@ -1,49 +1,51 @@
 
-class Position 
+module Vying
+  class Position 
 
-  attr_reader :game_id, :sequence_index, :sequence, :you
+    attr_reader :game_id, :sequence_index, :sequence, :you
 
-  def self.fetch( game_id, n=nil )
-    params = { :game_id => game_id }
-    params[:n] = n  if n
+    def self.fetch( game_id, n=nil )
+      params = { :game_id => game_id }
+      params[:n] = n  if n
 
-    r = Vying::Server.get( "/api/position", params )
-
-    if p = (r && r['position'])
-      p.instance_variable_set( "@game_id",        r['game_id'] )
-      p.instance_variable_set( "@sequence",       r['sequence'] )
-      p.instance_variable_set( "@sequence_index", r['n'] )
-      p.instance_variable_set( "@you",            r['you'] )
-    end
-
-    p
-  end
-
-  def submit_move( *moves )
-    if game_id && sequence_index
-      return false  if moves.empty?
-      return false  if SpecialMove[moves.first] && moves.length > 1
-
-      p = self
-      moves.each do |m|
-        return false  unless p.move?( m )
-        p = p.apply( m )
-      end
-
-      r = Vying::Server.post( "/api/move", 
-        :game_id => game_id, 
-        :n => sequence_index,
-        :moves => moves  )
+      r = Vying::Server.get( "/api/position", params )
 
       if p = (r && r['position'])
-        p.instance_variable_set( "@game_id", r['game_id'] )
+        p.instance_variable_set( "@game_id",        r['game_id'] )
+        p.instance_variable_set( "@sequence",       r['sequence'] )
         p.instance_variable_set( "@sequence_index", r['n'] )
+        p.instance_variable_set( "@you",            r['you'] )
       end
 
       p
     end
-  end
 
+    def submit_move( *moves )
+      if game_id && sequence_index
+        return false  if moves.empty?
+        return false  if SpecialMove[moves.first] && moves.length > 1
+
+        p = self
+        moves.each do |m|
+          return false  unless p.move?( m )
+          p = p.apply( m )
+        end
+
+        r = Vying::Server.post( "/api/move", 
+          :game_id => game_id, 
+          :n => sequence_index,
+          :moves => moves  )
+
+        if p = (r && r['position'])
+          p.instance_variable_set( "@game_id", r['game_id'] )
+          p.instance_variable_set( "@sequence_index", r['n'] )
+        end
+
+        p
+      end
+    end
+
+  end
 end
 
 Rules.list.each do |r| 
