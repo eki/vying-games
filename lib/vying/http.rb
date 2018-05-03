9cf5be845cf85d620e8cba022d687411ba1099ad
Eric K Idema <eki@vying.org>
2018-05-03 19:21:20 -0400

    Removing http client code.
    There used to be a server that you could connect to and run a bot to play the
    games. But, the server's been shutdown for just about forever, so let's remove
    this code.

--- lib/vying/http.rb
+++
@@ -1,9 +0,0 @@
-
-# frozen_string_literal: true
-
-require 'vying'
-require 'net/http'
-require 'vying/http/server'
-require 'vying/http/position'
-require 'vying/http/game'
-require 'vying/http/user'
